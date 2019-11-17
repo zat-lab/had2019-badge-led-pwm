4a83e358e84057a2fae293d5c13b8feb9e18af6a
Todd Zebert <Todd.Zebert@MilesPartnership.com>
2019-11-16 20:05:28 -0800

    more better working, comment and code cleanup

--- todd-v.v
+++ todd-v.v
@@ -1,31 +1,36 @@
 // Project entry point
 module top (
 	input  clk,
-	input [7:0] nbtn,
+	// input [7:0] nbtn, // @NOTE removed from sample as unconstrained with prod.lpf
 	output [10:0] ledc,
+	// output [3:0] leda,
 	// output [7:0] pmod,
-	output [29:0] genio, // An output reg foo is just shorthand for output foo_wire; reg foo; assign foo_wire = foo.
-
-	/*
-	input [29:0] genio_in,
-	output reg [29:0] genio_out,
-	output reg [29:0] genio_oe,
-	*/
 );
 
-	// PCB D11 is schem LED10 which is pin K20 which is lpf "ledc[6]" SITE "K20"
-	// PCB D12 is schem LED11 which is pin K19 which is lpft "ledc[7]" SITE "K19"    **** (ALSO "VIO" via 1K resistor)
+/* CORRECT AS OF had19_prod.lpf
+	PCB		Schem		Pin/Site	Logical		color
+	D5		LED1		E3				ledc[0]		RED
+	D6		LED2		D3				ledc[1]		BLUE
+	D7		LED3		C3				ledc[2]		RED
+	D8		LED4		C4				ledc[3]		BLUE
+	D9		LED5		C2				ledc[4]		RED
+	D10		LED6		B1				ledc[5]		BLUE
+
+
+	D11 	LED10		K20				ledc[6]
+	D12		LED11		K19				ledc[7]		GREEN
+	D13		LED7		B20				ledc[8]		GREEN
+	D14		LED8		B19				ledc[9]		RED
+	D15 	LED9		A18				ledc[10]	RED
+*/
+
+	// ******* BELOW ARE NOT CORRECT *******
+
+	// PCB D12 is schem LED11 which is pin K19 which is lpft "ledc[7]" SITE "K19"  **** ALSO PULLED UP via 1K resistor to "VIO" (3.3V)
 	// PCB D13 is schem LED7 which is pin B20 which is lpf "ledc[8]" SITE "B20";
 	// PCB D14 is schem LED8 which is pin B19 which is lpf "ledc[9]" SITE "B19";
 	// PCB D15 is schem LED9 which is pin A18 which is lpf *** missing *** try "leda[3]"
 
-
-	// Invert all buttons to make things easier
-	wire [7:0] btn = ~nbtn;
-
-	wire [29:0] gpio;
-
-	assign genio = gpio;
 
 	reg [7:0] top_led_counter = 0;
 	parameter max_top_led_counter = 5;
@@ -41,7 +46,7 @@ module top (
 	// Synchronous logic
 	always @(posedge clk) begin
 		// Clock divider pulse generator
-		if (clkdiv == 1000000) begin // @DEBUG was 800000
+		if (clkdiv == 2000000) begin // @DEBUG was 800000
 			clkdiv <= 0;
 			clkdiv_pulse <= 1;
 		end else begin
@@ -65,18 +70,18 @@ module top (
 			end else begin
 				top_led_counter <= top_led_counter + 1;
 			end
-			/*
-			if (top_led_counter == max_top_led_counter) begin
-				ledc[top_led_counter - 1] <= 0;
-			*/
 		end // if (clkdiv_pulse)
 
 
 
-		// ledc[9] <= 1; // @DEBUG
-		// ledc[8] <= 1; // @DEBUG
+		// @NOTE new correct code
+		// ledc[6] <= 1; // @DEBUG does nothing?! needs pads jumped!
+		// ledc[7] <= 1; // @DEBUG OK D12 GREEN
+		// ledc[8] <= 1; // @DEBUG OK D13 GREEN
+		// ledc[9] <= 1; // @DEBUG OK D14 GREEN
+		// ledc[10] <= 1; // @DEBUG OK D15 RED
 
-		// cycle through top LED's
+		// cycle through top LED's - @TODO needs to be changed to reflect reality
 		/*
 		if (clkdiv_pulse) begin
 			case (side_led_counter)
