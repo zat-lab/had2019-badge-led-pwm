2c7fcc37a3b1d2875a81c28381cf8e16f90682b6
Todd Zebert <Todd.Zebert@MilesPartnership.com>
2019-11-17 15:17:31 -0800

    it works

--- todd-v.v
+++ todd-v.v
@@ -61,7 +61,7 @@ module top (
 	// reg [7:0] a [0:3];
 	reg [7:0] leds [0:10] [0:2]; // 11 x 3 array of bytes ([7:0])
 
-	reg [0:2] color_index, color_index_nxt; // @DEBUG was = 0;
+	// reg [0:2] color_index, color_index_nxt; // @DEBUG was = 0;
 
 	// Clock divider and pulse registers
 	reg [20:0] clkdiv, clkdiv_nxt; // @DEBUG was = 0;
@@ -78,14 +78,6 @@ module top (
 	reg integer c;
 	reg integer s;
 
-	reg [24:0] cntr;
-
-
-	// ledrgb[0] <= 0; // @DEBUG works (as "normal")
-	// ledrgb[1] <= 0; // @DEBUG 0 + 1 yeilds blues into teal?
-	// ledrgb[2] <= 1; // @DEBUG 0 + 1 + 2 = all red cause it hogs all the current`
-
-
 
 	initial begin
 		// clear leds array
@@ -94,167 +86,104 @@ module top (
 				leds[l][c] = 0;
 			end
 		end
-	end
 
+		// @DEBUG testing LED colors below
+		leds[0][2'b00] = 'hff;
+		leds[1][2'b01] = 'hff;
+		leds[2][2'b10] = 'hff;
 
-	always @(posedge clk) begin
-		cntr <= cntr + 1;
+		leds[3][2'b00] = 'h80;
+		leds[4][2'b01] = 'h80;
+		leds[5][2'b10] = 'h80;
+
+		leds[6][2'b00] = 'h40;
+		leds[6][2'b01] = 'h40;
+		leds[7][2'b01] = 'h40;
+		leds[7][2'b10] = 'h40;
+		leds[8][2'b10] = 'h40;
+		leds[8][2'b00] = 'h40;
+
+		leds[9][2'b00] = 'h40;
+		leds[9][2'b01] = 'h40;
+		leds[9][2'b10] = 'h40;
+
+		leds[10][2'b00] = 'hff;
+		leds[10][2'b01] = 'hff;
+		leds[10][2'b10] = 'hff;
 	end
 
 
 	always @* begin
-		/* @DEBUG this results in
-		// ERROR: timing analysis failed due to presence of combinatorial loops, incomplete specification of timing ports, etc.
-		if (clkdiv == 255) begin // @DEBUG was 2000000 (for cycle code)
-			clkdiv_nxt <= 0;
-
-			// cycle color
-			if (color_index == 2) begin
-				color_index_nxt <= 0;
-			end else begin
-				color_index_nxt <= color_index + 1;
-			end;
-		end else begin
-			clkdiv_nxt <= clkdiv + 1;
-		end
-		*/
-
-		/* @DEBUG working(???)-ish
-		// cycle color
-		if (color_index == 2) begin
-			color_index_nxt <= 0;
-		end else begin
-			color_index_nxt <= color_index + 1;
-		end;
-
-		// Clock divider pulse generator
-		if (clkdiv == 256) begin // @DEBUG was 2000000 (for cycle code)
-			clkdiv_nxt <= 0;
-			// clkdiv_pulse <= 1;
-		end else begin
-			clkdiv_nxt <= clkdiv + 1;
-			// clkdiv_pulse <= 0;
-		end
-		*/
 
 		// Clock divider pulse generator
 		if (clkdiv == 0) begin // @DEBUG was 2000000 (for cycle code)
-			clkdiv_nxt <= 767; // 3 * 256 - 1
+			clkdiv_nxt = 'h2ff; // aka 3 * d256 - 1, will be count down! was 0
 		end else begin
-			clkdiv_nxt <= clkdiv - 1;
+			clkdiv_nxt = clkdiv - 1;
 		end
 
 		// set cycle of color "sinks"
-		// @TODO below original was color_index
-		// @DEBUG below was cntr[24:23]
 		case (clkdiv[9:8])
 			0: begin
-					ledrgb_nxt[0] <= 1;
-					ledrgb_nxt[1] <= 0;
-					ledrgb_nxt[2] <= 0;
+					ledrgb_nxt[0] = 1;
+					ledrgb_nxt[1] = 0;
+					ledrgb_nxt[2] = 0;
 				end
 			1: begin
-					ledrgb_nxt[0] <= 0;
-					ledrgb_nxt[1] <= 1;
-					ledrgb_nxt[2] <= 0;
+					ledrgb_nxt[0] = 0;
+					ledrgb_nxt[1] = 1;
+					ledrgb_nxt[2] = 0;
 				end
 			2: begin
-					ledrgb_nxt[0] <= 0;
-					ledrgb_nxt[1] <= 0;
-					ledrgb_nxt[2] <= 1;
+					ledrgb_nxt[0] = 0;
+					ledrgb_nxt[1] = 0;
+					ledrgb_nxt[2] = 1;
 				end
-			default: begin
-					ledrgb_nxt[0] <= 0;
-					ledrgb_nxt[1] <= 0;
-					ledrgb_nxt[2] <= 0;
+			default: begin // @ NOTE this should never be used...
+					ledrgb_nxt[0] = 0;
+					ledrgb_nxt[1] = 0;
+					ledrgb_nxt[2] = 0;
 				end
-			endcase
+		endcase
+
 
 		for (l = 0; l < (led_mapping_size + 1); l = l + 1) begin
 			// @TODO do color mapping
-			value = leds[l][color_index]; // @NOTE was <= which was wrong!
+			value = leds[l][clkdiv[9:8]];
 
-			// ledc[i] <= 1; // @DEBUG it does something!
-			if (clkdiv <= value) begin
-				ledc_nxt[l] <= 1;
+			if (clkdiv[7:0] < value) begin // just compare the lower 8 bits
+				ledc_nxt[l] = 1;
 			end else begin
-				ledc_nxt[l] <= 0;
+				ledc_nxt[l] = 0;
 			end
 
 		end // for
 
-	end
+	end // always @*
+
 
 	// Synchronous logic
 	always @(posedge clk) begin
 
-		// @DEBUG testing LED colors below
-		leds[0][2'b00] <= 'hff;
-		leds[1][2'b01] <= 'hff;
-		leds[2][2'b10] <= 'hff;
-
-
-		color_index <= color_index_nxt;
 		clkdiv <= clkdiv_nxt;
 
-
+		// set sink control
 		for (s = 0; s < 3; s = s + 1) begin
 			ledrgb[s] <= ledrgb_nxt[s];
 		end
 
-
+		// set LED source control
 		for (l = 0; l < (led_mapping_size + 1); l = l + 1) begin
 			ledc[l] <= ledc_nxt[l];
 		end
 
-
+		// "init"
 		if (!rst_) begin
-			clkdiv <= 767; // 3 * 256 - 1, will be count down! was 0
-			color_index <= 0;
+			clkdiv <= 'h2ff; // aka 3 * d256 - 1, will be count down! was 0
+			// color_index <= 0;
 		end
 
-
-		/*
-		// cycle through top LED's
-		if (clkdiv_pulse) begin
-			if (top_led_counter > min_top_led_counter) begin
-				ledc[top_led_counter - 1] <= 0;
-				ledc[top_led_counter] <= 1;
-			end else begin
-				ledc[max_top_led_counter] <= 0;
-				ledc[top_led_counter] <= 1;
-			end
-
-			if (top_led_counter == max_top_led_counter) begin
-				top_led_counter <= min_top_led_counter;
-			end else begin
-				top_led_counter <= top_led_counter + 1;
-			end
-		end // if (clkdiv_pulse)
-
-
-		// cycle through side LED's
-		if (clkdiv_pulse) begin
-			if (side_led_counter > min_side_led_counter) begin
-				ledc[side_led_counter - 1] <= 0;
-				ledc[side_led_counter] <= 1;
-			end else begin
-				ledc[max_side_led_counter] <= 0;
-				ledc[side_led_counter] <= 1;
-			end
-
-			if (side_led_counter == max_side_led_counter) begin
-				side_led_counter <= min_side_led_counter;
-			end else begin
-				side_led_counter <= side_led_counter + 1;
-			end
-		end // if (clkdiv_pulse)
-
-		*/
-
-
 	end // always @(posedge clk)
-
 
 
 endmodule
